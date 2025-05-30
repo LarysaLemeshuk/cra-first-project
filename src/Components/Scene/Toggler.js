9ad68dec935bdb167c808bb5b0dc220c1d55610f
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-05-30 21:39:07 +0200

    Child to parent

---
+++ src/Components/Scene/Toggler.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class Toggler extends React.Component {
+constructor(props){
+    super(props)
+
+    this.state ={
+        isOn: true
+    }
+}
+
+clickHandler(){
+    this.setState({
+        isOn: !this.state.isOn
+    })
+  this.props.changeParentState()
+}
+
+    render(){
+      
+    return(
+        <button onClick={() => {this.clickHandler()}}>{this.state.isOn ? 'On' : 'Off'}</button>
+    )
+}}
+
+export default Toggler
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AlohaDashboard from './Components/App/AlohaDashboard/AlohaDashboard';
 import TodoList from './Components/TodoList/TodoList';
-
+import Scene from './Components/Scene/Scene';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<TodoList />);
+root.render(< Scene/>);
