463c9d929ac0c32b675b61ccf6eef220f73d2a2b
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-06-01 19:03:31 +0200

    Task 1 resolve

---
+++ src/Components/UserList/UserCard.js
@@ -0,0 +1,16 @@
+// В компоненті UserCard потрібно реалізувати логіку відмалювання одного користувача
+// Цей користувач спускається в компоненту UserCard через пропси від батьківської компоненти UserList
+ import './style.css';
+
+ function UserCard(props){
+ const {user:{name:{first: firstName, last: lastName }, email, picture:{large: imgSrc}}}=props;
+
+ return(
+    <article className='card-wrapper'>
+        <img src={imgSrc} alt={`${firstName} ${lastName}`} className='user-img'/>
+        <h1>{firstName} {lastName}</h1>
+        <p>{email}</p>
+    </article>
+ )
+ }
+ export default UserCard;
--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -1,30 +1,29 @@
-import React from "react";
-import { getUsers } from "../../API";
+import React from 'react';
+import UserCard from './UserCard';
+import { getUsers } from '../../API';
 
 class UserList extends React.Component {
-constructor(props){
-    super(props)
-    this.state={
-        users:[]
-    }
-}
-componentDidMount(){
-getUsers().then(data =>{
-    const{results} = data;
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    getUsers().then((data) => {
+      const { results } = data;
 
-    this.setState({
-        users: results
-    })
-})
-}
+      this.setState({
+        users: results,
+      });
+    });
+  }
 
-render(){
-    return(
-        <ul>
+  render() {
+    const { users } = this.state;
 
-        </ul>
-    )
-}
+    return (<>{users.length  ? <UserCard user={users[0]} /> : null}</>)
+  }
 }
 
 export default UserList;
