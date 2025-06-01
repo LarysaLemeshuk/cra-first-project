844b2b2d23e1aac3947e54a7fa29d85bad6e6b73
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-06-01 17:03:42 +0200

    Counter(Lifecycle methods

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import AlohaDashboard from './Components/App/AlohaDashboard/AlohaDashboard';
 import TodoList from './Components/TodoList/TodoList';
 import Scene from './Components/Scene/Scene';
+import Counter from './Counter/Counter';
+import CounterPage from './Counter/CounterPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(< TodoList/>);
+root.render(<CounterPage/>);
