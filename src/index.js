d0d22561d6f54637d627b75727092bd6bc5e74e3
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-06-03 18:22:15 +0200

    Refoctor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AlohaDashboard from './Components/App/AlohaDashboard/AlohaDashboard';
-import TodoList from './Components/TodoList/TodoList';
-import Scene from './Components/Scene/Scene';
-import Counter from './Counter/Counter';
-import CounterPage from './Counter/CounterPage';
-import UserList from './Components/UserList/UserList';
+import App from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(< UserList/>);
+root.render(<App />);
