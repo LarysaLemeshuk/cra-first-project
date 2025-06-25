cde4843d21ddc3626441375312e406d09687a72f
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-06-25 21:24:48 +0200

    Add react-toastify + clipboard wrie text

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="colored"
+      />
       <button
         onClick={() => {
-          window.close();
+          window.location.assign('https://developer.mozilla.org/en-US/');
         }}
       >
-        Close page
+        {' '}
+        Assign
       </button>
-      ;
       <button
         onClick={() => {
-          window.open('https://developer.mozilla.org/en-US/');
+          window.location.reload();
         }}
       >
-        Close GH
+        {' '}
+        Reload
       </button>
-      ;
+      <button
+        onClick={() => {
+          window.location.replace(
+            'https://www.youtube.com/watch?v=48KV1SJFH78'
+          );
+        }}
+      >
+        {' '}
+        Replace
+      </button>
+
+      <p>lorem ipsum dolor</p>
+      <button onClick={() => {
+          window.navigator.clipboard.writeText('orem ipsum dolo');
+          toast.success('🦄Copied to clipboard')
+        }}
+      > Click to copi text</button>
     </>
   );
 }
