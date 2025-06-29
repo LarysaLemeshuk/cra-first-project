83c1cd5c2d8fb8eb988ac35814a04b0e3ed343a6
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-06-29 16:18:16 +0200

    React router dom welcome

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import WindowResizer from './WindowResizer/WindowResizer';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Counter from './Counter/CounterPage';
+import UserList from './UserList/UserList';
+import TodoList from './TodoList/TodoList';
+import Home from './Home/Home';
+import NotFound from './NotFound/NotFound';
 
 function App() {
   return (
-    <>
-      <WindowResizer />
-    </>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="counter">Go to Counter component</Link>
+          </li>
+          <li>
+            <Link to="users">Go to UserList component</Link>
+          </li>
+          <li>
+            <Link to="todo">Go to TodoList component</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/todo" element={<TodoList />} />
+        <Route path='/*' element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
+
+// 1. SSR (server-side rendering) - додатки, які рендеряться на сервері
+// 2. CSR (client-side-rendering) - додатки, які рендеряться на клієнті
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <h1>
+            404
+            <p>Page not found 🤬</p>
+        </h1>
+    );
+}
+
+export default NotFound;
