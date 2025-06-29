4eed5ac367f81c96405b0f6de2eb3daa35e28a8c
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-06-29 13:41:02 +0200

    WindowResizer

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,76 +1,10 @@
 import React from 'react';
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import WindowResizer from './WindowResizer/WindowResizer';
 
 function App() {
   return (
     <>
-      <ToastContainer
-        position="top-center"
-        autoClose={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        theme="colored"
-      />
-      <button
-        onClick={() => {
-          window.location.assign('https://developer.mozilla.org/en-US/');
-        }}
-      >
-        {' '}
-        Assign
-      </button>
-      <button
-        onClick={() => {
-          window.location.reload();
-        }}
-      >
-        {' '}
-        Reload
-      </button>
-      <button
-        onClick={() => {
-          window.location.replace(
-            'https://www.youtube.com/watch?v=48KV1SJFH78'
-          );
-        }}
-      >
-        {' '}
-        Replace
-      </button>
-
-      <p>lorem ipsum dolor</p>
-      <button
-        onClick={() => {
-          window.navigator.clipboard.writeText('orem ipsum dolo');
-          toast.success('🦄Copied to clipboard');
-        }}
-      >
-        {' '}
-        Click to copi text
-      </button>
-      <button
-        onClick={() => {
-          window.navigator.clipboard.readText().then((text) => {
-            console.log(text);
-          });
-        }}
-      >
-        {' '}
-        Read text from buffer
-      </button>
-      <button
-        onClick={() => {
-          window.navigator.geolocation.getCurrentPosition((positionObject)=>{
-            console.log(positionObject);
-          })
-        }}
-      > Get my current position
-      </button>
+      <WindowResizer />
     </>
   );
 }
