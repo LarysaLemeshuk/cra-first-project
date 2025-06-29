a0c2db2794f6541ef6cb62c3110b29d9f4573c35
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-06-29 19:07:43 +0200

    Render prop patern

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,39 +1,37 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Counter from './Counter/CounterPage';
-import UserList from './UserList/UserList';
-import TodoList from './TodoList/TodoList';
-import Home from './Home/Home';
-import NotFound from './NotFound/NotFound';
+import PhonesLoader from './PhonesLoader/PhonesLoader';
+import TVLoader from './TVLoader/TvLoader';
+import DataProvider from './DataProvider/DataProvider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="counter">Go to Counter component</Link>
-          </li>
-          <li>
-            <Link to="users">Go to UserList component</Link>
-          </li>
-          <li>
-            <Link to="todo">Go to TodoList component</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <DataProvider
+        loadData={() => {
+          return fetch('./phones.json').then((response) => response.json());
+        }}
+      >
+        {(state) => {
+          const { data, isLoading, isError } = state;
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/users" element={<UserList />} />
-        <Route path="/todo" element={<TodoList />} />
-        <Route path='/*' element={<NotFound/>}/>
-      </Routes>
-    </BrowserRouter>
+          return (
+            <>
+              {isLoading && <div>Loading......</div>}
+              {isError && <div>Error happening: {isError.message}</div>}
+
+              <ul>
+                {data.map((data, index) => (
+                  <li key={index}>
+                    Brand: {data.brand} --- Model: {data.model} --- Price:
+                    {data.price}
+                  </li>
+                ))}
+              </ul>
+            </>
+          );
+        }}
+      </DataProvider>
+    </>
   );
 }
 export default App;
-
-// 1. SSR (server-side rendering) - додатки, які рендеряться на сервері
-// 2. CSR (client-side-rendering) - додатки, які рендеряться на клієнті
