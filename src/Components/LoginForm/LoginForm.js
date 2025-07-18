e580400396f0579cc4769eefa5b65ff16de9e58f
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-07-18 17:37:42 +0200

    Formik validation + error message

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { SIGN_UP_SCHEMA } from '../../schemas';
 
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const initialState = {
   firsName: '',
@@ -16,14 +16,22 @@ function SignUpForm(props) {
     actions.resetForm();
   };
   return (
-    <Formik initialValues={initialState} onSubmit={handelSubmitToFormik}>
+    <Formik
+      initialValues={initialState}
+      onSubmit={handelSubmitToFormik}
+      validationSchema={SIGN_UP_SCHEMA}
+    >
       {(formikProps) => {
         return (
-          <Form>
+          <Form style={{ display: 'flex', flexDirection: 'column' }}>
             <Field name="firstName" placeholder="Type your firstName" />
+            <ErrorMessage name="firstName" component="p" />
             <Field name="lastName" placeholder="Type your lastName" />
+            <ErrorMessage name="lastName" component="p" />
             <Field name="email" placeholder="Type your email" />
+            <ErrorMessage name="email" component="p" />
             <Field name="password" placeholder="Type your password" />
+            <ErrorMessage name="password" component="p" />
             <button type="submit">Send</button>
           </Form>
         );
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,9 +3,12 @@ import * as yup from 'yup';
 export const SIGN_UP_SCHEMA = yup.object({
   firstName: yup.string().required().min(1).max(50),
   lastName: yup.string().required().min(1).max(50),
-  email: yup.string().required().email(),
+  email: yup.string().required().email('Email is not valid'),
   password: yup
     .string()
     .required()
-    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/),
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+      'Password is not valid '
+    ),
 });
