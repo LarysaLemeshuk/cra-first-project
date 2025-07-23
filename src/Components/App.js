2db407a5fbbe4f5946a5de9536402e5713c179cc
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-07-23 17:27:59 +0200

    useRef

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,58 +1,9 @@
 import React from 'react';
-
-import Tree from './Tree/Tree';
-import { ThemeContext } from '../contexts/themeContext';
-import { UserContext } from '../contexts/userContext';
-import CONSTANTS from '../constants';
-
-const { THEMES } = CONSTANTS;
+import Square from './Square/Square';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: {
-        firstName: 'John',
-        lastName: ' Doe',
-        email: 'johndoe@gmail.com',
-        avatar:
-          'https://www.havahart.com/media/wysiwyg/hh/cms/lc/dogs/hh-animals-dog-1.png',
-      },
-      theme: THEMES.LIGHT,
-    };
-  }
-
-  logOut = () => {
-    this.setState({
-      user: {},
-    });
-  };
-
-  setTheme = (theme) => {
-    this.setState({
-      theme,
-    });
-  };
   render() {
-    return (
-      <ThemeContext.Provider
-        value={{
-          theme: this.state.theme,
-          setTheme: this.setTheme,
-        }}
-      >
-        <UserContext.Provider
-          value={{
-            user: this.state.user,
-            logOut: this.logOut,
-          }}
-        >
-          App
-          <Tree />
-        </UserContext.Provider>
-      </ThemeContext.Provider>
-    );
+    return <Square />;
   }
 }
 
