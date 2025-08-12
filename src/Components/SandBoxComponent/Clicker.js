192afff55ef47bba07d12e639b664391df5ff11b
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-08-12 18:23:18 +0200

    useCallback practise task resolve

--- src/Components/SandBoxComponent/Clicker.js
+++ src/Components/SandBoxComponent/Clicker.js
@@ -4,19 +4,53 @@ import { useState, useCallback } from 'react';
 
 const Clicker = () => {
   const [clickCount, setClickCount] = useState(0);
+  const [inputValue, setInputValue] = useState('');
 
   //   const clickHandler = () => {
   //     setClickCount(clickCount + 1);
   //   };
 
   const clickHandler = useCallback(() => {
-    setClickCount(clickCount => clickCount + 1);
+    setClickCount((clickCount) => clickCount + 1);
   }, []);
+
+  //   const changeHandler = ({ target: { value } }) => {
+  //     setInputValue(Number(value));
+  //   };
+
+  const changeHandler = useCallback(({ target: { value } }) => {
+    setInputValue(Number(value));
+  }, []);
+
+  //   const logValueHandler = () => {
+  //     let sum = 0;
+  //     for (let i = 0; i < inputValue; i++) {
+  //       sum += i;
+  //     }
+  //     console.log(sum);
+  //     console.log(inputValue);
+  //   };
+
+  const logValueHandler = useCallback(() => {
+    let sum = 0;
+    for (let i = 0; i < inputValue; i++) {
+      sum += i;
+    }
+    console.log(sum);
+    console.log(inputValue);
+  }, [inputValue]);
 
   return (
     <>
       <h1>{clickCount}</h1>
       <button onClick={clickHandler}>Click</button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={changeHandler}
+        placeholder="Введіть число, для якого потрібно знайти суму чисел"
+      />
+      <button onClick={logValueHandler}>Розрахувати суму </button>
     </>
   );
 };
@@ -35,4 +69,10 @@ const memoizedCallback = useCallback(
 
 
 
+*/
+
+/*
+Використайте useCallback для оптимізації.
+Огорніть logValueHandler та changeHandler y useCallback.
+Використайте вже мемоізовану функцію.
 */
